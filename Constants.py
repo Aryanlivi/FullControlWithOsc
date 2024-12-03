c58e060fd5c5a78780ced6d7a2be3b211a132946
AnkitShrestha-Xenserv <aankitshrestha@gmail.com>
2024-12-03 19:59:35 +0545

    feat: add a threshold for visible points before predicting action - a visibility threshold that determines if the point is visible or not - a threshold for the number of points that should be visible before an action is predicted

--- Constants.py
+++ Constants.py
@@ -23,17 +23,22 @@ CLASSES_LIST = ['Jump',
 # Directory to save csv files
 OUTPUT_DIR = 'Output'
 
-MODEL='model_2.h5'
+MODEL='FullControlWithOsc\model.h5'
 
 # Dataframe column header for a single video
 custom_headers = ['Frame Number', 'x','y','z','Visibility']
 
 # Define predefined parameters of the model
-max_num_frames = 20  # maximum number of frames per action sequence
+max_num_frames = 15  # maximum number of frames per action sequence
 num_landmarks = 23    # number of landmarks per frame
 num_features = 4      # number of features per landmark (x, y, z, visibility)
 num_classes = len(CLASSES_LIST)       # number of action classes
 
+# How many points must be visible before the prediction can start
+VISIBLE_POINTS_REQUIREMENT = 14
+# How much confidence mediapipe requires before a point is considered as visible
+VISIBILITY_THRESHOLD = 0.6
+# How much confidence our model requires before an action is classified
 PREDICTION_CONFIDENCE=0.8
 DEBOUNCE_TIME=0.6
 exclude_points = [1,2,3,4,5,6,7,8,9,10]
